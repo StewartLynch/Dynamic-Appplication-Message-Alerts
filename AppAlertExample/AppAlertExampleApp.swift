3ba563facdb009bb25184a2dd8111085b5e9b987
Stewart Lynch <slynch@createchsol.com>
2023-11-15 08:46:11 -0800

    Initial Commit

---
+++ AppAlertExample/AppAlertExampleApp.swift
@@ -0,0 +1,21 @@
+//
+// Created for AppAlertExample
+// by  Stewart Lynch on 2023-11-15
+//
+// Follow me on Mastodon: @StewartLynch@iosdev.space
+// Follow me on Threads: @StewartLynch (https://www.threads.net)
+// Follow me on X: https://x.com/StewartLynch
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+
+
+import SwiftUI
+
+@main
+struct AppAlertExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
